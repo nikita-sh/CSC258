a9ed934118d0686a026727cc23b0f599ce4862ab
Shumeiko Nikita <shumeik2@teach.cs.toronto.edu>
2018-09-29 19:47:13 -0400

    Added Verilog file for Full and Ripple Carry Adders

--- Lab3/7mux1/mux7to1.do
+++ Lab3/7mux1/mux7to1.do
@@ -1,14 +1,14 @@
 # Set the working dir, where all compiled Verilog goes.
 vlib work
 
-# Compile all Verilog modules in mux.v to working dir;
+# Compile all Verilog modules in mux7to1.v to working dir;
 # could also have multiple Verilog files.
 # The timescale argument defines default time unit
 # (used when no unit is specified), while the second number
 # defines precision (all times are rounded to this value)
 vlog -timescale 1ns/1ns mux7to1.v
 
-# Load simulation using mux as the top level simulation module.
+# Load simulation using mux7to1 as the top level simulation module.
 vsim mux7to1
 
 log {/*}
---
+++ Lab3/RippleCarryAdder/rippleCarryAdder.v
@@ -0,0 +1,61 @@
+//Ripple Carry Adder
+
+module rippleCarryAdder(a, b, a1, b1, a2, b2, a3, b3, cin, s, s1, s2, s3, cout);
+	input a;
+	input b;
+	input a1;
+	input b1;
+	input a2;
+	input b2;
+	input a3;
+	input b3;
+	input cin;
+	output s;
+	output s1;
+	output s2;
+	output s3;
+	output cout;
+	
+	wire c1;
+	wire c2;
+	wire c3;
+	
+	fullAdder add(
+				.a(a),
+				.b(b),
+				.cin(cin),
+				.cout(c1),
+				.s(s));
+				
+	fullAdder add1(
+				.a(a1),
+				.b(b1),
+				.cin(c1),
+				.cout(c2),
+				.s(s1));
+				
+	fullAdder add2(
+				.a(a2),
+				.b(b2),
+				.cin(c2),
+				.cout(c3),
+				.s(s2));
+				
+	fullAdder add3(
+				.a(a3),
+				.b(b3),
+				.cin(c3),
+				.cout(cout),
+				.s(s3));
+endmodule
+
+module fullAdder(a, b, cin, cout, s);
+	input a;
+	input b;
+	input cin;
+	output cout;
+	output s;
+	
+	assign cout = (a & b) | (a & c) | (b & c)
+	assign s = (a & ~b & ~cin) | (~a & b & ~cin) | (a & b & cin) | (~a & ~b & cin)
+endmodule 
